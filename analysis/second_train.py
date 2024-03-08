29cb83cff919a0dcbbc053fa0c0056329f22ed31
JihoonBrianJun <jay@upbit.asia-northeast3-a.c.weighty-diagram-411700.internal>
2024-03-08 08:34:49 +0000

    Modified codes related to CELoss computation

--- analysis/second_train.py
+++ analysis/second_train.py
@@ -19,6 +19,16 @@ def main(args):
     
     print(f'src shape: {src.shape}')
     print(f'tgt shape: {tgt.shape}')
+
+    pos_label = len(np.where(tgt[:,-1,0]>=args.value_threshold)[0])
+    neg_label = len(np.where(tgt[:,-1,0]<=-args.value_threshold)[0])
+    print(f'Pos Label: {pos_label}\n Neg Label: {neg_label}')
+
+    ce_weights = torch.tensor([tgt.shape[0]/pos_label,
+                            tgt.shape[0]/(tgt.shape[0]-pos_label-neg_label),
+                            tgt.shape[0]/neg_label])
+    ce_weights = nn.functional.normalize(ce_weights, dim=0, p=1)
+    print(f'ce_weights: {ce_weights}')
     
     data = np.array([{'src': src[i], 'tgt': tgt[i]} for i in range(src.shape[0])])
     train_idx = np.random.choice(np.arange(src.shape[0]), size=int(src.shape[0]*args.train_ratio), replace=False)
@@ -53,9 +63,7 @@ def main(args):
     print(f'model param size: {num_param}')
     
     # loss_function = nn.MSELoss()
-    loss_function = nn.CrossEntropyLoss(weight=torch.tensor([args.anomaly_weight,
-                                                             1-2*args.anomaly_weight,
-                                                             args.anomaly_weight]).to(device))
+    loss_function = nn.CrossEntropyLoss(weight=ce_weights.to(device))
     optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=0.0)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     
@@ -88,6 +96,7 @@ def main(args):
     model.eval()
     test_loss = 0
     correct = 0
+    real_correct, real_tgt = 0, 0
     for idx, batch in tqdm(enumerate(test_loader)):
         src = batch['src'].to(torch.float32).to(device)
         tgt = batch['tgt'].to(torch.float32).to(device)
@@ -101,8 +110,14 @@ def main(args):
         test_loss += loss.detach().cpu().item()
         # correct += ((out[:,-1]*label[:,-1])>0).sum().item()
         correct += (torch.argmax(out[:,-1,:],dim=1)==label[:,-1]).sum().item()
+        real_tgt += (label[:,-1]!=1).to(torch.long).sum().item()
+        real_correct += ((label[:,-1]!=1).to(torch.long) * (torch.argmax(out[:,-1,:],dim=1)==label[:,-1]).to(torch.long)).sum().item()
+
+        if idx == 0:
+            print(f'Out: {out[:,-1,:]}\n Label: {label[:,-1]}')
     print(f'Test Average Loss: {test_loss / (idx+1)}')
     print(f'Test Correct: {correct} out of {args.bs*(tgt.shape[1]-1)*(idx+1)}')
+    print(f'Test Real Correct: {real_correct} out of {real_tgt}')
     
     
 if __name__ == '__main__':
@@ -116,11 +131,10 @@ if __name__ == '__main__':
     parser.add_argument('--gpu', type=bool, default=False)
     parser.add_argument('--epoch', type=int, default=1)
     parser.add_argument('--bs', type=int, default=16)
-    parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--gamma', type=int, default=0.1)
+    parser.add_argument('--lr', type=float, default=1e-5)
+    parser.add_argument('--gamma', type=float, default=1)
     parser.add_argument('--train_ratio', type=float, default=0.9)
     parser.add_argument('--tgt_clip_value', type=float, default=1)
     parser.add_argument('--value_threshold', type=float, default=1)
-    parser.add_argument('--anomaly_weight', type=float, default=0.4)
     args = parser.parse_args()
     main(args)
