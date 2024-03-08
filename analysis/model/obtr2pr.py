72e210eb2fae6b0fdf6b5a0b016403b2daf080e0
JihoonBrianJun <jay@upbit.asia-northeast3-a.c.weighty-diagram-411700.internal>
2024-03-08 06:51:41 +0000

    Modified Model & Training

--- analysis/model/obtr2pr.py
+++ analysis/model/obtr2pr.py
@@ -59,3 +59,41 @@ class OrderbookTrade2Price(nn.Module):
         tgt = self.tgt_pos_emb(tgt)
         out = self.transformer(src, tgt, src_mask=src_mask, tgt_mask=tgt_mask)
         return self.out_proj(out).view(out.shape[0],-1)
+
+
+class AnomalyDetector(nn.Module):
+    def __init__(self, model_dim, n_head, num_layers, src_dim, tgt_dim, src_len, tgt_len, result_dim):
+        super().__init__()
+        self.src_proj = nn.Linear(src_dim, model_dim)
+        self.src_pos_emb = PositionalEncoding(d_model=model_dim,
+                                              max_len=src_len)
+        
+        self.tgt_proj = nn.Linear(tgt_dim, model_dim)
+        self.tgt_pos_emb = PositionalEncoding(d_model=model_dim,
+                                              max_len=tgt_len)
+        
+        self.transformer = Transformer(d_model=model_dim,
+                                       nhead=n_head,
+                                       num_encoder_layers=num_layers,
+                                       num_decoder_layers=num_layers,
+                                       dim_feedforward=model_dim*4,
+                                       batch_first=True)
+        self.out_proj = nn.Linear(model_dim, result_dim)
+        self.softmax = nn.Softmax(dim=2)
+    
+    def forward(self, src, tgt, src_mask=None, tgt_mask=None):
+        if type(src) != torch.Tensor:
+            src = torch.tensor(src)
+        src = self.src_proj(src)
+        src = self.src_pos_emb(src)
+        
+        if src_mask is not None:
+            src_mask = src_mask.to(src.device)
+        if tgt_mask is None:
+            tgt_mask = Transformer.generate_square_subsequent_mask(tgt.shape[1])
+        tgt_mask = tgt_mask.to(tgt.device)
+        
+        tgt = self.tgt_proj(tgt)
+        tgt = self.tgt_pos_emb(tgt)
+        out = self.transformer(src, tgt, src_mask=src_mask, tgt_mask=tgt_mask)
+        return self.softmax(self.out_proj(out))
--- analysis/second_train.py
+++ analysis/second_train.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import StepLR
-from model.obtr2pr import OrderbookTrade2Price
+from model.obtr2pr import OrderbookTrade2Price, AnomalyDetector
 
 
 def main(args):
@@ -32,19 +32,30 @@ def main(args):
     else:
         device = torch.device("cpu")
         
-    model = OrderbookTrade2Price(model_dim=args.model_dim,
-                                 n_head=args.n_head,
-                                 num_layers=args.num_layers,
-                                 src_dim=src.shape[-1],
-                                 tgt_dim=tgt.shape[-1],
-                                 src_len=src.shape[-2],
-                                 tgt_len=tgt.shape[-2]).to(device)
+    # model = OrderbookTrade2Price(model_dim=args.model_dim,
+    #                              n_head=args.n_head,
+    #                              num_layers=args.num_layers,
+    #                              src_dim=src.shape[-1],
+    #                              tgt_dim=tgt.shape[-1],
+    #                              src_len=src.shape[-2],
+    #                              tgt_len=tgt.shape[-2]).to(device)
+    model = AnomalyDetector(model_dim=args.model_dim,
+                            n_head=args.n_head,
+                            num_layers=args.num_layers,
+                            src_dim=src.shape[-1],
+                            tgt_dim=tgt.shape[-1],
+                            src_len=src.shape[-2],
+                            tgt_len=tgt.shape[-2],
+                            result_dim=args.result_dim).to(device)
     num_param = 0
     for _, param in model.named_parameters():
         num_param += param.numel()
     print(f'model param size: {num_param}')
     
-    loss_function = nn.MSELoss()
+    # loss_function = nn.MSELoss()
+    loss_function = nn.CrossEntropyLoss(weight=torch.tensor([args.anomaly_weight,
+                                                             1-2*args.anomaly_weight,
+                                                             args.anomaly_weight]).to(device))
     optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=0.0)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     
@@ -52,14 +63,16 @@ def main(args):
         epoch_loss = 0
         for idx, batch in tqdm(enumerate(train_loader)):
             src = batch['src'].to(torch.float32).to(device)
-            tgt = torch.clamp(torch.nan_to_num(batch['tgt']),
+            tgt = torch.clamp(batch['tgt'],
                               min=-args.tgt_clip_value, 
                               max=args.tgt_clip_value).to(torch.float32).to(device)
             
             out = model(src, tgt[:,:-1,:])
             label = tgt[:,1:,:].squeeze(dim=2)
+            label = 1+(label>=args.value_threshold).to(torch.long)-(label<=-args.value_threshold).to(torch.long)
             
-            loss = loss_function(out[:,-1], label[:,-1])
+            # loss = loss_function(out[:,-1], label[:,-1])
+            loss = loss_function(out[:,-1,:],label[:,-1])
             epoch_loss += loss.detach().cpu().item()
             loss.backward()
                     
@@ -77,14 +90,17 @@ def main(args):
     correct = 0
     for idx, batch in tqdm(enumerate(test_loader)):
         src = batch['src'].to(torch.float32).to(device)
-        tgt = torch.nan_to_num(batch['tgt']).to(torch.float32).to(device)
+        tgt = batch['tgt'].to(torch.float32).to(device)
         
         out = model(src, tgt[:,:-1,:])
         label = tgt[:,1:,:].squeeze(dim=2)
+        label = 1+(label>=args.value_threshold).to(torch.long)-(label<=-args.value_threshold).to(torch.long)
         
-        loss = loss_function(out[:,-1], label[:,-1])
+        # loss = loss_function(out[:,-1], label[:,-1])
+        loss = loss_function(out[:,-1,:],label[:,-1])
         test_loss += loss.detach().cpu().item()
-        correct += ((out[:,-1]*label[:,-1])>0).sum().item()
+        # correct += ((out[:,-1]*label[:,-1])>0).sum().item()
+        correct += (torch.argmax(out[:,-1,:],dim=1)==label[:,-1]).sum().item()
     print(f'Test Average Loss: {test_loss / (idx+1)}')
     print(f'Test Correct: {correct} out of {args.bs*(tgt.shape[1]-1)*(idx+1)}')
     
@@ -93,6 +109,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='/home/jay/UpbitTrade/analysis/data/train')
     parser.add_argument('--save_dir', type=str, default='/home/jay/UpbitTrade/analysis/vanilla.pt')
+    parser.add_argument('--result_dim', type=int, default=3)
     parser.add_argument('--model_dim', type=int, default=64)
     parser.add_argument('--n_head', type=int, default=2)
     parser.add_argument('--num_layers', type=int, default=2)
@@ -103,5 +120,7 @@ if __name__ == '__main__':
     parser.add_argument('--gamma', type=int, default=0.1)
     parser.add_argument('--train_ratio', type=float, default=0.9)
     parser.add_argument('--tgt_clip_value', type=float, default=1)
+    parser.add_argument('--value_threshold', type=float, default=1)
+    parser.add_argument('--anomaly_weight', type=float, default=0.4)
     args = parser.parse_args()
     main(args)
