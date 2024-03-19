538339e18addec2e14d2f7730114480f77a5fe62
JihoonBrianJun <jhjun0530@snu.ac.kr>
2024-03-19 16:12:01 +0900

    Revert "Fixed minor error in testing code"
    This reverts commit c9463b9f7a88a4f56dd9d6f81ff3726065c0e62c.

--- train_minute.py
+++ train_minute.py
@@ -117,10 +117,7 @@ def main(args):
                                   max=args.tgt_clip_value).to(torch.float32).to(device)
                 
                 for step in range(pred_len):
-                    if step == 0:
-                        out = model(ob, tr, volume, tgt[:,:data_len,:])
-                    else:
-                        out = model(ob, tr, volume, torch.cat((tgt[:,:data_len,:], out[:,-step:].unsqueeze(dim=2)),dim=1))
+                    out = model(ob, tr, volume, tgt[:,:data_len+step,:])
 
                 label = tgt[:,1:,:].squeeze(dim=2)                    
                 loss = loss_function(out,label)
