f8167420d55f57aa5d751510fa44cffea27a618a
JihoonBrianJun <jhjun0530@snu.ac.kr>
2024-03-19 16:11:47 +0900

    Revert "Fixed parameter updating error while training"
    This reverts commit 5d9049ac9179681ebc2a2086c44d646f946ee753.

--- train_minute.py
+++ train_minute.py
@@ -91,11 +91,12 @@ def main(args):
                 label = tgt[:,1:data_len+step+1,:].squeeze(dim=2)
                 loss = loss_function(out,label)
                 loss.backward()
-
-                optimizer.step()
-                optimizer.zero_grad()
             
-            epoch_loss += loss.detach().cpu().item()        
+            epoch_loss += loss.detach().cpu().item()
+                    
+            optimizer.step()
+            optimizer.zero_grad()
+        
         print(f'Epoch {epoch} Average Loss: {epoch_loss/(idx+1)}')
         scheduler.step()    
     
